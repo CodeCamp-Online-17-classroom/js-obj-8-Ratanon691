13d7aa45fc5cc13f0dab1c8a8a2ce1443661ff2f
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-25 22:02:39 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-// write code here
+let menu = {
+  width: 200,
+  height: 300,
+  title: 'My menu'
+};
+
+function multiply(obj, x) {
+  let result = {}
+for(key in obj){
+  if(typeof obj[key] === `number`){
+  result[key] = obj[key] * x
+  }else{
+    result[key] = obj[key]
+  }  
+}
+  return result
+}
